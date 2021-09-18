55d609025a362cb4275e752ef7c0c0ca115d7d17
Zebulaun <77033324+Zebulaun@users.noreply.github.com>
2021-09-18 15:21:30 -0700

    Add files via upload

---
+++ 8values.github.io-master/ideologies.js
@@ -0,0 +1,56 @@
+ideologies = [
+    {
+        "name": "Classical Coleist",
+        "stats": {
+            "econ": 80,
+            "dipl": 25,
+            "govt": 30,
+            "scty": 90
+        }
+    },
+    {
+        "name": "Libertarian Coleist",
+        "stats": {
+            "econ": 65,
+            "dipl": 40,
+            "govt": 60,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Oliverist",
+        "stats": {
+            "econ": 50,
+            "dipl": 60,
+            "govt": 80,
+            "scty": 70
+        }
+    },
+    {
+        "name": "Chad Cultist",
+        "stats": {
+            "econ": 0,
+            "dipl": 100,
+            "govt": 20,
+            "scty": 30
+        }
+    },
+    {
+        "name": "Anarcho-Chadist",
+        "stats": {
+            "econ": 30,
+            "dipl": 90,
+            "govt": 70,
+            "scty": 30
+        }
+    },
+    {
+        "name": "True Griller",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 0
+        }
+    }
+];
