470d2006914e31524892e432319cf2f5455ea77a
Zebulaun <77033324+Zebulaun@users.noreply.github.com>
2021-09-18 15:22:37 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,146 @@
+questions = [
+    {
+        "question": "Oppression by chadness is more of a concern than oppression by state.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is necessary for the state to intervene in the chadness hierarchy to protect members.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The freer the chad hierarchy, the freer the server.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is better to maintain an efficient chad-scale than to ensure equality for all members.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It's wrong to call someone cringe.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "We should enforce policy through direct action.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Equality among members (socially) is more important than Owen's self-esteem.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "amogus",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Owen's happiness must be redistributed to others.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Some people are cringe and some people are not. That's just how it is.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "The stronger the server state is, the more oppressive.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 10,
+            "scty": -10
+        }
+    },
+    {
+        "question": "I am very interested in political ideology.",
+        "effect": {
+            "econ": 5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "You win is the original server, which had more history and is clearly superior.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Communism is cringe and no one should be allowed to talk about it",
+        "effect": {
+            "econ": -10,
+            "dipl": 5,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "this is a cry for help btw",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "'We win' is inferior to the classic server.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    }
+];
